83219859c7638dc73e5798d612af9b5ce56a91bc
Yasom09 <yaseminerduran02@gmail.com>
2023-10-29 23:05:25 +0100

    deneme99

---
+++ doublesayilar.java
@@ -0,0 +1,14 @@
+public class doublesayilar {
+public static void main (String[]args) { 
+int a=19;
+int b=3;
+int c=2;
+double x = a / b;
+double y = a / c;
+double z = a / (double)c;
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+ }
+    
+}
